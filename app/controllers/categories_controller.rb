9bbab9e0a7c874cbb45df833b22c00e0f6448698
Tomas Van Roose <Tomas.VanRoose@UGent.be>
2016-04-16 16:27:54 +0200

    Add CRUD for categories
    - Only Create and Index because the rest is not needed.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,24 @@
 class CategoriesController < ApplicationController
 
-	def new
+	def index
+		@categories = Category.all
+	end
 
+	def new
+		@category = Category.new
+	end
+
+	def create
+		@category = Category.new(category_params)
+		@category.save
+		redirect_to root_path
+	end
+
+
+
+	private
+	def category_params
+		params.require(:category).permit(:name)
 	end
 
 
