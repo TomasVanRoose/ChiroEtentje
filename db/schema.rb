3888355c4b2fdb10e6c9ee91902b5dc9695d8ee1
Tomas Van Roose <Tomas.VanRoose@UGent.be>
2016-04-17 16:15:02 +0200

    Add users model and controller
    - Create a workable navbar
    - Model for groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416143157) do
+ActiveRecord::Schema.define(version: 20160417141348) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "table_number"
+    t.string   "name"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "products", force: :cascade do |t|
@@ -25,6 +32,12 @@ ActiveRecord::Schema.define(version: 20160416143157) do
     t.string   "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
